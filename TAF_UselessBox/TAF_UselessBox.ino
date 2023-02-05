c9fc2fe6680e24d301834658268cba816c2de773
Ahmed Khattab <cuperous@gmail.com>
2023-02-05 15:25:16 -0800

    Add code to move arm fast or slow randomly

--- TAF_UselessBox/TAF_UselessBox.ino
+++ TAF_UselessBox/TAF_UselessBox.ino
@@ -1,9 +1,52 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "Servo.h"
 
+int servo_pin_lid = D8;  // for ESP8266 microcontroller
+int servo_pin_arm = D0;  // for ESP8266 microcontroller
+
+Servo myservolid;
+Servo myservoarm;
+int angle = 5;  
+int randomNumber;
+
+void setup() 
+{
+  myservoarm.attach(servo_pin_arm, 500, 2400);
+  pinMode(D5, INPUT);
+  myservoarm.write(30);
+  delay(1000);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+   if ((digitalRead(D5)== LOW)) {
+     randomNumber=random(500);
+      if(randomNumber > 250)
+        moveArmFast();
+      else
+        moveArmSlow();
+  } 
+}
 
+void moveArmSlow()
+{
+  for(int i=30;i<160;i++)
+  {
+      myservoarm.write(i);
+      delay(10);
+  }
+  delay(1000);
+  for(int i=140;i>30;i--)
+  {
+      myservoarm.write(i);
+      delay(10);
+  }
+}
+
+void moveArmFast()
+{
+  delay(5000);
+  myservoarm.write(160);
+  delay(1000);
+  myservoarm.write(30);
+  delay(1000);
 }
